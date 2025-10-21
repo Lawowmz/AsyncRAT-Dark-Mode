f281ef3f7f4ca59a36e08643b28d4ea3f02feb66
Denis T. <deadspell21391@gmail.com>
2025-10-21 11:56:47 +0200

    AssemblyInfo.cs

---
+++ AsyncRAT-C#/MessagePack/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.72.494.566")]
+[assembly: AssemblyFileVersion("1.72.494.566")]
